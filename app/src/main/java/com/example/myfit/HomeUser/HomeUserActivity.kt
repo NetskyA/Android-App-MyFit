af792b66c4416eec3ccf898fb849780ff4c355ce
Geovann Chandra <103976069+gechdr@users.noreply.github.com>
2024-04-21 14:18:22 +0700

    Update

--- app/src/main/java/com/example/myfit/HomeUser/HomeUserActivity.kt
+++ app/src/main/java/com/example/myfit/HomeUser/HomeUserActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfit.HomeUser
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentContainerView
 import com.example.myfit.R
@@ -10,6 +11,7 @@ class HomeUserActivity : AppCompatActivity() {
     lateinit var container:FragmentContainerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_home_user)
         container = findViewById(R.id.fragmentContainer)
     }
