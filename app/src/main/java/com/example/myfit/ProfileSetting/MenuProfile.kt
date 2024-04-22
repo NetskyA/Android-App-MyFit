a2345686f45cce5a574e4fd0a8855b5367850d0c
Aldi <aldiaafn16@gmail.com>
2024-04-22 16:57:43 +0700

    menu profilr

--- app/src/main/java/com/example/myfit/ProfileSetting/MenuProfile.kt
+++ app/src/main/java/com/example/myfit/ProfileSetting/MenuProfile.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myfit.R
+import com.example.myfit.databinding.FragmentMenuProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,16 +23,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MenuProfile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentMenuProfileBinding
+    private lateinit var recyclerViewContent: RecyclerView
+    private lateinit var menuProfileAdapter: MenuProfileAdapter
+    private lateinit var layoutManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,31 +37,25 @@ class MenuProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_profile, container, false)
+        binding = FragmentMenuProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-    }
+        var temp:ArrayList<String> = arrayListOf("test", "test2", "test3","test", "test2", "test3","test", "test2", "test3","test", "test2", "test3")
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuProfile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuProfile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        recyclerViewContent = binding.rvFeedcontent
+        layoutManager = GridLayoutManager(context, 2)
+        menuProfileAdapter = MenuProfileAdapter(temp)
+        recyclerViewContent.adapter = menuProfileAdapter
+        recyclerViewContent.layoutManager = layoutManager
+
+        /*binding.iv_menusetting.setOnClickListener {
+            val stepmove = MenuProfile.actionGlobalLogin()
+            findNavController().navigate(stepmove)
+        }*/
+
     }
 }
