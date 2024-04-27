4e6b1038e2bf85d0005b32868d1176b9ae4f4727
Geovann Chandra <103976069+gechdr@users.noreply.github.com>
2024-04-27 10:14:13 +0700

    Fix Profile Navigation

--- app/src/main/java/com/example/myfit/ProfileSetting/MenuProfileSettingV2.kt
+++ app/src/main/java/com/example/myfit/ProfileSetting/MenuProfileSettingV2.kt
@@ -11,27 +11,21 @@ import com.example.myfit.databinding.ActivityMenuProfileSettingV2Binding
 
 class MenuProfileSettingV2 : AppCompatActivity() {
 
-    lateinit var container: FragmentContainerView
     private lateinit var binding: ActivityMenuProfileSettingV2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_profile_setting_v2)
-        container = findViewById(R.id.ProfileFragmentContainer)
 
         binding = ActivityMenuProfileSettingV2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.ibAccount.setOnClickListener {
-            Toast.makeText(this, "Account", Toast.LENGTH_SHORT).show()
-//            findNavController(R.id.fragmentContainerView2).navigate(R.id.action_global_profileSettingAccount)
-            findNavController(container.id).navigate(R.id.action_global_profileSettingAccount)
+            findNavController(R.id.fragmentContainerView2).navigate(R.id.action_global_profileSettingAccount)
         }
 
         binding.ibBody.setOnClickListener {
-            Toast.makeText(this, "Body", Toast.LENGTH_SHORT).show()
-            findNavController(container.id).navigate(R.id.action_global_profileSettingBody)
+            findNavController(R.id.fragmentContainerView2).navigate(R.id.action_global_profileSettingBody)
         }
     }
 }
