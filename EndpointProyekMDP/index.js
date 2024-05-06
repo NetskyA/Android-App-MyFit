a3027e41140ed7d7a436fb4ca17b8b3afc91b4d0
Alvin Bernard Wiyono <alvinbwiyono@gmail.com>
2024-05-06 17:11:20 +0700

    Update index.js

--- EndpointProyekMDP/index.js
+++ EndpointProyekMDP/index.js
@@ -11,3 +11,32 @@ app.use("/api", router);
 app.listen(3000, () =>
   console.log(`Server is running at http://localhost:3000`)
 );
+app.get("/test",(req,res)=>{
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "alvinbwiyono@gmail.com",
+      pass: "jqzfirqblwjgdwtk",
+    },
+  });
+  
+  var mailOptions = {
+    from: 'alvinbwiyono@gmail.com',
+    to: 'alvinbwiyono2@gmail.com',
+    subject: 'Sending Email using Node.js',
+    html: '<h1>Welcome</h1><p>That was easy!</p>'
+  }; 
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      return res.send("err")
+    } else {
+      console.log('Email sent: ' + info.response);
+      return res.send("sukses")
+    }
+  });
+})
