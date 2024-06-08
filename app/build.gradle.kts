d5ade0ebd9b2496a01292b6a346747c0f01c4db5
Alvin Bernard Wiyono <alvinbwiyono@gmail.com>
2024-06-08 21:06:10 +0700

    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "id.ac.istts.myfit"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
